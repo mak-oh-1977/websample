5aa7a3516b3282a03fc29b9e12ffe59ded1ed015
ohnuma <mak.oh.1977@gmail.com>
2017-06-11 22:57:30 +0900

    DB定義追加

---
+++ db.sql
@@ -0,0 +1,24 @@
+﻿USE [test]
+GO
+
+/****** Object:  Table [dbo].[users]    Script Date: 2017/06/11 22:55:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[users](
+	[id] [int] NOT NULL,
+	[name] [nchar](20) NOT NULL,
+	[kana] [nchar](20) NULL,
+	[tel] [nchar](20) NULL,
+	[password] [nchar](20) NULL,
+ CONSTRAINT [PK_users] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
