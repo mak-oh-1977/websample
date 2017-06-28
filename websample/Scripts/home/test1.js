9f8efb5c96cf8c65f277ecde0a8efe430d2a2578
ohnuma <mak.oh.1977@gmail.com>
2017-06-29 06:51:10 +0900

    ラジオボタンの取得

--- websample/Scripts/home/test1.js
+++ websample/Scripts/home/test1.js
@@ -1,12 +1,41 @@
 ﻿$(document).ready(function () {
     $('[tabindex=1]').focus();
-})
+});
 
-$('[tabindex]').keypress(function (e) {
+$('.in-item').keydown(function (e) {
     if (e.which == 13) {
         var idx = $(this).attr('tabindex');
         idx++;
         $('[tabindex=' + idx + ']').focus();
     }
-})
+});
 
+$('button#send').click(function () {
+
+    var items = $('.in-item');
+    for (var i = 0; i < items.length; i++)
+    {
+        var item = $(items).eq(i);
+        var type = $(item).attr('type');
+
+        var v = '';
+        var name = $(item).attr('name');
+        if (type == 'radio')
+        {
+            var check = $(item).prop('checked');
+            if (check == true)
+            {
+                v = $(item).val();
+            }
+        }
+        else
+        {
+            v = $(item).val()
+        }
+        if (v != '')
+            alert(name + ':' + v);
+
+    }
+    var radio = $('input[type]:checked').val();
+    alert(radio);
+});
