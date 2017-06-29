c3c760f88ecb695e8f00ceb432ecee8aae15335f
ohnuma <mak.oh.1977@gmail.com>
2017-06-30 06:55:14 +0900

    ドロップダウンリストの実装

--- websample/Controllers/homeController.cs
+++ websample/Controllers/homeController.cs
@@ -17,6 +17,13 @@ namespace websample.Controllers
         // GET: test1
         public ActionResult test1()
         {
+            ViewBag.SelectOptions = new SelectListItem[] {
+                new SelectListItem() { Value="2016", Text="平成28年" },
+                new SelectListItem() { Value="2017", Text="平成29年" },
+                new SelectListItem() { Value="2018", Text="平成30年" },
+                new SelectListItem() { Value="2019", Text="平成31年" },
+            };
+
             return View();
         }
     }
